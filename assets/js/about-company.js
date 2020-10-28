04456034e0a9c657bd9ac73eb5c25370ced91c46
OdrinskayaMargarita <margarita.odrinskaya@gmail.com>
2020-10-28 16:25:11 +0200

    28.10 abput company - tab review

--- assets/js/about-company.js
+++ assets/js/about-company.js
@@ -1,5 +1,54 @@
 $(document).ready(function(){
 
+  
+  $( ".tabs-company > li" ).click(function(event) {
+    var attr1 = $(this).data('tab');
+    var text1 = $(this).text();
+    console.log(attr1)
+
+    $('.fourth-crumb').css('display','none')
+
+    $('.third-crumb > a').html(text1)
+    $(".tabs-company > li").removeClass('active')
+    $(this).addClass('active');
+    $('.one-tab').removeClass('active-tab');
+    $('main').find('.' + attr1).addClass('active-tab')
+
+    if( $('#review-tab').hasClass('active-tab') ){
+      $('.fourth-crumb').css('display','block')
+    }
+
+  });
+
+  
+  $( ".phis-ur-tab > p" ).click(function(event) {
+    var attr2 = $(this).data('review');
+    var text2 = $(this).text();
+
+    $('.fourth-crumb > a').html(text2)
+
+    $(".phis-ur-tab > p").removeClass('active-phis-ur-tab')
+    $(this).addClass('active-phis-ur-tab');
+
+    $('.in-rev-tab').removeClass('active-rev-tab');
+    $('.tab3').find('.' + attr2).addClass('active-rev-tab')
+
+  });
+
+
+  
+  $( ".open-text-review" ).click(function(event) {
+
+    $(this).parent().parent().find('.hidden-text').addClass('show-hidden-text')
+
+  });
+  
+  $( ".close-hidden-text" ).click(function(event) {
+
+    $(this).parent().parent().removeClass('show-hidden-text')
+
+  });
+
   $('.all-history-slider').slick({
     dots: true,
     infinite: false,
@@ -34,5 +83,6 @@ $(document).ready(function(){
       },
     ]
   });
+
 
 });
